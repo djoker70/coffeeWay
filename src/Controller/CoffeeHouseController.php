b10aabc8defd5d595dcc9d640d53e91a81ef797b
djoker70 <djoker72@gmail.com>
2021-12-29 13:31:11 +0300

    add cs fixer. phpStan

--- src/Controller/CoffeeHouseController.php
+++ src/Controller/CoffeeHouseController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-;
+
 class CoffeeHouseController extends AbstractController
 {
     #[Route('/', name: 'index')]
